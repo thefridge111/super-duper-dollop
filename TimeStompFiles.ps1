5feda15e4cd0579adb1cf79a5ddea6ba2394bfb9
Will Perry <wfperry1@gmail.com>
2022-06-07 13:52:44 -0400

    Adds files for blackjack and file manipulation.

---
+++ TimeStompFiles.ps1
@@ -0,0 +1,10 @@
+﻿$modifyfiles = Get-ChildItem -force | Where-Object {! $_.PSIsContainer}
+foreach($object in $modifyfiles)
+{
+$object.CreationTime=("11/11/2011 12:00:00")
+
+$object.LastAccessTime=("11/11/2011 12:00:00")
+
+$object.LastWritetime=("11/11/2011 12:00:00")
+
+}
