a9188cd045ee5232267267902abdf2594cba6466
Will Perry <wfperry1@gmail.com>
2022-06-08 09:24:11 -0400

    Duplicates functions from util to all scripts due to module resolution problems

--- files/file-artifacts.ps1
+++ files/file-artifacts.ps1
@@ -1,6 +1,53 @@
-Import-Module ..\util\flag-util.psm1
-
 CopyFlag -flagName "flag1.ps1" -destBaseDir "C:\" -destDirName "Temp" -destName "game.exe"
 CopyFlag -flagName "flag2.ps1" -destBaseDir  "C:\Users\" -destDirName  "YankeeTwoTwo" -destName "explore.exe"
 
 CreateScheduledFlag -name "svchast.ext" -username "Administrator" -taskToExecute "C:\Users\YankeeTwoTwo\explore.exe" -atStart
+
+Function CopyFlag {
+    param (
+        [string] [Parameter(Mandatory=$false)] $flagDirectory,
+        [string] [Parameter(Mandatory)] $flagName,
+        [string] [Parameter(Mandatory)] $destBaseDir,
+        [string] [Parameter(Mandatory)] $destDirName,
+        [string] [Parameter(Mandatory)] $destName
+    )
+    
+    if ($flagDirectory -eq $null) {
+        $flagDirectory = Get-Location
+    }
+
+    if ((Test-Path -Path "${destBaseDir}${destDirName}") -eq $false) {
+        New-Item -Path $destBaseDir -Name $destDirName -ItemType "directory"
+    }
+
+    $flagLocation = "${flagDirectory}${flagName}"
+    $flagDestination = "${destBaseDir}${destDirName}\${destName}"
+
+    Copy-Item $flagLocation -Destination $flagDestination
+}
+
+Function CreateScheduledFlag {
+    param (
+        [switch][Parameter(Mandatory=$false)] $random,
+        [switch][Parameter(Mandatory=$false)] $atStart,
+        [string][Parameter(Mandatory=$false)] $frequency,
+        [string][Parameter(Mandatory=$false)] $atTime,
+        [string][Parameter(Mandatory)] $name,
+        [string][Parameter(Mandatory)] $userName,
+        [string][Parameter(Mandatory)] $taskToExecute
+    )
+    if ($atStart) {
+        $Time = New-ScheduledTaskTrigger -AtStartup
+    } elseif ($random) {
+        $Time = New-ScheduledTaskTrigger -RepetitionInterval "01:00" -RandomDelay "00:20"
+    } elseif ($frequency -gt 0) {
+        $Time = New-ScheduledTaskTrigger -RepetitionInterval $frequency
+    } elseif ($atTime -ne "") {
+        $Time = New-ScheduledTaskTrigger -At $atTime
+    } else {
+        $Time = New-ScheduledTaskTrigger -At 12:00 -Once
+    }
+
+    $task = New-ScheduledTaskAction -Execute $taskToExecute
+    Register-ScheduledTask -TaskName $name -Trigger $Time -User $userName -Action $task
+}
--- scheduledTasks/cmd.ps1
+++ scheduledTasks/cmd.ps1
@@ -1,3 +1,27 @@
-Import-Module ..\util\flag-util.psm1
-
 CreateScheduledFlag -atTime "10:00" -name "cyber" -username "Administrator" -taskToExecute "cmd"
+
+Function CreateScheduledFlag {
+    param (
+        [switch][Parameter(Mandatory=$false)] $random,
+        [switch][Parameter(Mandatory=$false)] $atStart,
+        [string][Parameter(Mandatory=$false)] $frequency,
+        [string][Parameter(Mandatory=$false)] $atTime,
+        [string][Parameter(Mandatory)] $name,
+        [string][Parameter(Mandatory)] $userName,
+        [string][Parameter(Mandatory)] $taskToExecute
+    )
+    if ($atStart) {
+        $Time = New-ScheduledTaskTrigger -AtStartup
+    } elseif ($random) {
+        $Time = New-ScheduledTaskTrigger -RepetitionInterval "01:00" -RandomDelay "00:20"
+    } elseif ($frequency -gt 0) {
+        $Time = New-ScheduledTaskTrigger -RepetitionInterval $frequency
+    } elseif ($atTime -ne "") {
+        $Time = New-ScheduledTaskTrigger -At $atTime
+    } else {
+        $Time = New-ScheduledTaskTrigger -At 12:00 -Once
+    }
+
+    $task = New-ScheduledTaskAction -Execute $taskToExecute
+    Register-ScheduledTask -TaskName $name -Trigger $Time -User $userName -Action $task
+}
