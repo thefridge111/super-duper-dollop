5ca41fee06ece557acc97d3615c94ccd74041102
cook-remington <remington.cook1@gmail.com>
2022-06-09 12:47:31 -0400

    Removed excess comments and added information to the read me section of the script.

--- Registry.ps1
+++ Registry.ps1
@@ -3,7 +3,8 @@
 <#
     
     The goal of this script is to fill several startup locations in the registry with an artifact.
-
+    If a registry key is not present it is okay, whatever registry keys are present will still populate. Ignore any errors that occur if "The path is does not exist", or something similar.
+    This script must be ran as administrator. If an error like "Requested registry access is not allowed" occurs, that means you are not running this script with the proper permissions.
 
 #>
 
@@ -32,41 +33,7 @@ If($option -eq "A")
     New-ItemProperty -Path "HKLM:\Software\Microsoft\Active Setup\Installed Components" -Name "w32.Assarm" -Value "C:\Windows\System32\Fun.exe" -PropertyType String -Force | Out-Null
     New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" -Name "W32.Assarm" -Value "C:\Windows\System32\SVIQ.exe" -PropertyType String -Force | Out-Null
     New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\UserDefaults" -Name "W32.Assarm" -Value "C:\Windows\System32\WinSit.exe" -PropertyType String -Force | Out-Null
-        #### Checking If The Keys Are Present ####
-           # Doesn't Work #
-           <#
-            Write-Host "Loading...."
-            Start-Sleep 1
-            Write-Host "..."
-            Start-Sleep 1
-            
-                $CuRun = Get-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\Powershell"
-                $CuRunOnce = Get-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\WATCHTHIS"
-                $CuRunServices = Get-ItemProperty "HKCU:\Software\Classes\*\ShellEx\ContextMenuHandlers\Big-Deal"
-                $CuRunServicesOnce = Get-ItemProperty "HKCU:\Software\Classes\Directory\ShellEx\ContextMenuHandlers\Test"
-                $CuWindows = Get-ItemProperty "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows\I-wouldn't-delete-this-if-I-were-you."
-                $wannacry = Get-ItemProperty "HKCU\Software\WanaCrypt0r\wd\qeriuwjhrf"
-                $HkRun = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\Powershell"
-                $HkRun = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\WATCHTHIS"
-                $HkRun = Get-ItemProperty "HKLM:\Software\Microsoft\Active Setup\Installed Components\Please-Dont-Delete"
-                $HkRun = Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders\Common Startup\Kyle"
-                $HkRun = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\UserDefaults\Test"
-                Write-Host "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\Powershell = $CuRun"
-                Write-Host "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\WATCHTHIS = $CuRunOnce"
-                Write-Host "HKCU:\Software\Classes\*\ShellEx\ContextMenuHandlers\Big-Deal = $CuRunServices"
-                Write-Host "HKCU:\Software\Classes\Directory\ShellEx\ContextMenuHandlers\Test = $CuRunServicesOnce"
-                Write-Host "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows\I-wouldn't-delete-this-if-I-were-you. = $CuWindows"
-                Write-Host "HKCU\Software\WanaCrypt0r\wd\qeriuwjhrf = $wannacry"
-                Write-Host "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\powershell = $HKRUN"
-                Write-Host "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\WATCHTHIS = $HKRUNONCE"
-                Write-Host "HKLM:\Software\Microsoft\Active Setup\Installed Components\Please-Dont-Delete = $HKRUNSERV"
-                Write-Host "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders\Common Startup\Kyle = $HKRUNSERVONCE"
-                Write-Host "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\UserDefaults\Test = $HKWinlogon"
-
-
-                Read-Host
-                
-                #>
+       
 }
 
 
@@ -89,38 +56,7 @@ elseif($option -eq "R")
     Remove-ItemProperty -Path "HKLM:\Software\Microsoft\Active Setup\Installed Components" -Name "w32.Assarm"
     Remove-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" -Name "W32.Assarm"
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\UserDefaults" -Name "W32.Assarm"
-        #### Checking If The Keys Are Present ####
-           # Doesn't Work #
-           <#
-            Write-Host "Loading...."
-            Start-Sleep 1
-            Write-Host "..."
-            Start-Sleep 1
-                $CuRun = Get-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\Powershell"
-                $CuRunOnce = Get-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\WATCHTHIS"
-                $CuRunServices = Get-ItemProperty "HKCU:\Software\Classes\*\ShellEx\ContextMenuHandlers\Big-Deal"
-                $CuRunServicesOnce = Get-ItemProperty "HKCU:\Software\Classes\Directory\ShellEx\ContextMenuHandlers\Test"
-                $CuWindows = Get-ItemProperty "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows\I-wouldn't-delete-this-if-I-were-you."
-                $wannacry = Get-ItemProperty "HKCU\Software\WanaCrypt0r\wd\qeriuwjhrf"
-                $HkRun = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\Powershell"
-                $HkRun = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\WATCHTHIS"
-                $HkRun = Get-ItemProperty "HKLM:\Software\Microsoft\Active Setup\Installed Components\Please-Dont-Delete"
-                $HkRun = Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders\Common Startup\Kyle"
-                $HkRun = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\UserDefaults\Test"
-                Write-Host "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\Powershell = $CuRun"
-                Write-Host "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\WATCHTHIS = $CuRunOnce"
-                Write-Host "HKCU:\Software\Classes\*\ShellEx\ContextMenuHandlers\Big-Deal = $CuRunServices"
-                Write-Host "HKCU:\Software\Classes\Directory\ShellEx\ContextMenuHandlers\Test = $CuRunServicesOnce"
-                Write-Host "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows\I-wouldn't-delete-this-if-I-were-you. = $CuWindows"
-                Write-Host "HKCU\Software\WanaCrypt0r\wd\qeriuwjhrf = $wannacry"
-                Write-Host "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\powershell = $HKRUN"
-                Write-Host "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\WATCHTHIS = $HKRUNONCE"
-                Write-Host "HKLM:\Software\Microsoft\Active Setup\Installed Components\Please-Dont-Delete = $HKRUNSERV"
-                Write-Host "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders\Common Startup\Kyle = $HKRUNSERVONCE"
-                Write-Host "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\UserDefaults\Test = $HKWinlogon"
 
-                Read-Host
-                #>
 }
 
 
@@ -128,18 +64,3 @@ Else
 {
 Write-Host "Invalid Input"
 }
-
-
-<#
-[HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run]
-[HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce]
-[HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunServices]
-[HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce]
-[HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\Userinit]
-
-[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run]
-[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce]
-[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServices]
-[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce]
-[HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Windows]
-#>
